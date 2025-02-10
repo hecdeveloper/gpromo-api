66cefbce77dbebcf18fc0ab11492418adc860a4e
Hector <hector.dev.ui@gmail.com>
2025-02-09 22:10:13 -0600

    update catalogos

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,25 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface CatalogoCatalogocard extends Schema.Component {
+  collectionName: 'components_catalogo_catalogocards';
+  info: {
+    displayName: 'catalogocard';
+    description: '';
+  };
+  attributes: {
+    titulo: Attribute.String & Attribute.Required;
+    imagen: Attribute.Media<'images'>;
+    tituloboton: Attribute.String &
+      Attribute.Required &
+      Attribute.DefaultTo<'VER CAT\u00C1LOGO'>;
+    linkboton: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'catalogo.catalogocard': CatalogoCatalogocard;
+    }
+  }
 }
