4acf615fe6cc9f6b9699c6678bc0d59f7809dc7d
ManulSV <manul@example.com>
2025-02-09 21:06:44 -0600

    Se agregaron imagenes de catalogos y estructura de pagina y componestes de catologos

---
+++ src/api/catalogospagina/controllers/catalogospagina.ts
@@ -0,0 +1,7 @@
+/**
+ * catalogospagina controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::catalogospagina.catalogospagina');
---
+++ src/api/catalogospagina/routes/catalogospagina.ts
@@ -0,0 +1,7 @@
+/**
+ * catalogospagina router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::catalogospagina.catalogospagina');
